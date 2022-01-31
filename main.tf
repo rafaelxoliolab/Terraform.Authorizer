03f3902022e3f20c65d538ec676919db823df664
Rafael Xolio <rafael.xolio@definityfirst.com>
2022-01-31 01:27:06 -0600

    readding main

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+#change ami with the ami from AWS EC2 instance
+resource "aws_instance" "web"{
+    ami= "ami-XXXXxxx"
+    instance_type = "t2.micro"
+
+    tags = {
+      Name = "Terraform Example Auth"
+    }
+}
+
+module "aws_auth_function" {
+  source = "./modules/authorizer"
+  api_id = aws_api_gateway_rest_api.ApiGateway.id
+  lambda_name = "dealers-auth"
+  runtime = "nodejs14.x"
+  handler = "index.handler"
+  output_path = "./modules/authorizer/aws-authorizer.zip"
+}
